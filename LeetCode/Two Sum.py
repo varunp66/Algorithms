a5fd296a79635179a92e5146279ba2d8110fb1a6
Varun Pathak <varun_yashu88@yahoo.com>
2018-05-18 23:09:39 -0400

    Delete Two Sum.py

--- LeetCode/Two Sum.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def twoSum(self, nums, target):
-        hashed = {}
-        for i, n in enumerate(nums):
-            if target-n in hashed: return [hashed[target-n], i]
-            hashed[n] = i
